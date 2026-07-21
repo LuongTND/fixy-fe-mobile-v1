5b2aadc591468dc1e8b5929e84fd29f97183f4b5
DaiLT <letandai1304@gmail.com>
2026-07-21 16:02:29 +0700

    fix: resolve Hermes compilation crash on startup

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,11 @@
 module.exports = function (api) {
   api.cache(true);
-  let plugins = [];
+  let plugins = [
+    '@babel/plugin-transform-class-properties',
+    '@babel/plugin-transform-private-methods',
+    '@babel/plugin-transform-private-property-in-object',
+    '@babel/plugin-transform-classes',
+  ];
 
   plugins.push('react-native-worklets/plugin');
 
